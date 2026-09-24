251e04061ea24adb1c1882ae9cfd36ff4bfa6be2
nuranihomes <anurani@nuranihomes.com>
2026-09-24 19:11:28 +0400

    Add buyer and partner interest forms

---
+++ form-endpoint.js
@@ -0,0 +1,3 @@
+// Paste the Google Apps Script web app URL between the quotes.
+// Both the buyer and partner forms send their answers here.
+window.NH_FORM_ENDPOINT = "https://script.google.com/macros/s/AKfycbzSkibUvBPQgw2qXrL62Uo3HMJXlP6hBXpvzaEdZUdiC6BY72x5iOTI2kr1tt4NUPZ9/exec";
